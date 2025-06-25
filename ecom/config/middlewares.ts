b43395171bbc22a4ec330d9cdf4fba395423a191
dhruvjai45 <dhruvjain.gdsc@gmail.com>
2025-06-25 14:11:17 +0530

    basic changes

--- ecom/config/middlewares.ts
+++ ecom/config/middlewares.ts
@@ -11,7 +11,7 @@ module.exports = [
         'http://localhost:1337', // Strapi admin
         'http://localhost:9002', // Next.js app
         'http://127.0.0.1:9002', // Fallback for localhost
-        'https://controlled-preventing-diamond-aye.trycloudflare.com', // Strapi API
+        'https://cathedral-johns-representative-good.trycloudflare.com', // Strapi API
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       keepHeaderOnError: true,
