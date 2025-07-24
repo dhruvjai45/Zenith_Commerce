68b8609ff97d4d3f83adc16dbd8a7d8e53be5d86
dhruvjai45 <dhruvjain.gdsc@gmail.com>
2025-07-24 11:19:36 +0530

    server changes

--- ecom/config/server.ts
+++ ecom/config/server.ts
@@ -1,10 +1,11 @@
 // config/server.ts
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
+  url: env('PUBLIC_URL', 'https://zenith-commerce.onrender.com'), // Public URL for Render deployment
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS'), // Replace with your secure keys
   },
   webhooks: {
     populateRelations: false,
