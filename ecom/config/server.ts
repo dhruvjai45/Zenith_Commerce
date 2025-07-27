cf1f99b2f8eb52e4cb41ee3e82f654f2baedd1c3
dhruvjai45 <dhruvjain.gdsc@gmail.com>
2025-07-27 15:14:54 +0530

    server and middlewares

--- ecom/config/server.ts
+++ ecom/config/server.ts
@@ -1,9 +1,8 @@
-
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 10000),
   url: env('RENDER_EXTERNAL_URL', 'https://zenith-commerce.onrender.com'),
-  proxy: env.bool('PROXY_ENABLED', true), // This line is crucial
+  proxy: env.bool('PROXY_ENABLED', true), // important when using HTTPS through Render/Vercel
   app: {
     keys: env.array('APP_KEYS'),
   },
